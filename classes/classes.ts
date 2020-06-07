80e5309b2eca7304dc8fc3fd2d03f4688b0fdc5a
felipe <profissionalf.andre@gmail.com>
2020-06-07 15:50:27 -0300

    feat: new updates by paste classes

--- classes/classes.ts
+++ classes/classes.ts
@@ -34,3 +34,147 @@ console.log(aniversarioEsperto)
 const casamentoEsperto = new DataEsperta
 casamentoEsperto.ano = 2017
 console.log(casamentoEsperto)
+class Carro {
+    private velocidadeAtual: number = 0
+
+    constructor(
+        public marca:string,
+        public modelo:string,
+        private velocidadeMaxima:number= 200
+    ) {}
+    protected alterarVelocidade(delta:number) : number {
+        const novaVelocidade = this.velocidadeAtual + delta
+        const velocidadeValida = novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima
+
+        if(velocidadeValida) {
+            this.velocidadeAtual = novaVelocidade
+        }else{
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+        }
+        return this.velocidadeAtual
+    }
+    public acelerar() : number {
+        return this.alterarVelocidade(5)
+    }
+    public frear():number {
+        return this.alterarVelocidade(-5)
+    }
+}
+
+const carr1 = new Carro('Ford','Ka',185)
+
+class Ferrari extends Carro {
+    constructor(modelo: string,velocidadeMaxima: number) {
+        super('Ferrari',modelo,velocidadeMaxima)
+    }
+    public acelerar() : number {
+        return this.alterarVelocidade(20)
+    }
+    public frear():number {
+        return this.alterarVelocidade(-15)
+    }
+}
+const f40 = new Ferrari('F40',324)
+console.log(`${f40.marca}`)
+console.log(`${f40.modelo}`)
+console.log(`${f40.acelerar()}`)
+console.log(`${f40.frear()}`)
+
+// getters & setters
+
+class Pessoa {
+    private _idade:number = 0
+    get idade():number {
+        return this._idade
+    }
+    set idade(valor:number){
+        if(valor >= 0 && valor <= 120)  {
+            this._idade = valor
+        }
+    }
+}
+const pessoa1 = new Pessoa
+pessoa1.idade = 10
+console.log(pessoa1)
+console.log(pessoa1.idade)
+
+pessoa1.idade = -3
+console.log(pessoa1)
+
+// Atributos e metodos estaticos
+
+class Matematica{
+    // PI: number = 3.14
+    static PI: number = 3.14
+
+    // areaCirc(raio:number):number {
+    //     return this.PI * raio * raio
+    // }
+    static areaCirc(raio:number):number {
+        return Matematica.PI * raio * raio
+    }
+}
+
+console.log(Matematica.areaCirc(4))
+
+// classe abstrata
+
+// abstract class X {
+//     abstract y(a:number):number
+//     abstract w(b:number):void 
+// }
+
+abstract class Calculo {
+    protected resultado : number = 0
+    abstract executar(...calculo:number[]):void
+
+    public getResultado() : number {
+        return this.resultado
+    }
+}
+class Soma extends Calculo{
+    public executar(...calculo:number[]):void {
+        this.resultado = calculo.reduce((t,a) => t+a)
+    }
+}
+class Multiplicacao extends Calculo{
+    public executar(...calculo:number[]):void {
+        this.resultado = calculo.reduce((t,a) => t*a)
+    }
+}
+
+let c1: Calculo = new Soma()
+c1.executar(2,3,4,5)
+console.log(c1.getResultado())
+
+let c2 = new Multiplicacao()
+c2.executar(2,3,4,5)
+console.log(c2.getResultado())
+
+class Unico {
+    private static instance: Unico = new Unico
+    private constructor(){}
+    static getInstance(): Unico{
+        return Unico.instance
+        return this.instance
+    }
+    public agora(){
+        return new Date
+    }
+}
+// const errado = new Unico
+console.log(Unico.getInstance().agora)
+
+// Somente leitura
+
+class Aviao{
+    public readonly modelo:string
+    constructor(modelo: string, public readonly prefixo:string) {
+        this.modelo = modelo
+    }
+}
+const turboHelice = new Aviao('Tu-114','PT-ABC')
+// turboHelice.modelo = 'DC-8'
+// turboHelice.prefixo = 'PT-ABC'
+
+console.log(turboHelice)
--- classes/desafio.ts
+++ classes/desafio.ts
@@ -4,7 +4,102 @@ class Produto {
         public preco: number,
         public desconto: number = 0
     ){}
+    
+    // precoComDesconto(preco: number = this.preco, desconto: number = this.desconto): number {
+    //     let res = desconto * 100
+    //     preco = res
+    //     return preco
+    // }
+    precoComDesconto() : number {
+        return this.preco * (1-this.desconto)
+    }
+
+    public resumo(): any {
+        return `${this.nome} custa ${this.precoComDesconto()} (${this.desconto * 100}% off)`
+    }
 }
 let product = new Produto('Maquina de lavar',600)
-let product2 = new Produto('Nootbook',1500,0.5)
-console.log(product, product2)
+let product2 = new Produto('Nootbook',1000,0.20)
+console.log(product2.resumo())
+// console.log(product2.precoComDesconto(1000,0.2))
+
+// desafio 2
+
+// Exercício 1 - Classe
+// function Moto(nome) {
+//     this.nome = nome
+//     this.velocidade = 0
+ 
+//     this.buzinar = function() {
+//         console.log('Toooooooooot!')
+//     }
+ 
+//     this.acelerar= function(delta) {
+//         this.velocidade = this.velocidade + delta
+//     }
+// }
+ 
+// var moto = new Moto('Ducati')
+// moto.buzinar()
+// console.log(moto.velocidade)
+// moto.acelerar(30)
+// console.log(moto.velocidade)
+
+class Moto {
+   constructor(
+    public name:string='Felipe',
+    public velocidade:number=0
+   ){}
+   public buzinar = () : void => {
+    console.log('Toooooooooot!')
+   }
+   public acelerar = (delta:number) : any => {
+        this.velocidade = this.velocidade + delta
+   }
+}
+
+const moto = new Moto('Ducati')
+moto.buzinar()
+console.log(moto.velocidade,'Velocidade do metodo public velocidade')
+moto.acelerar(30)
+console.log(moto.velocidade,'Velocidade 30')
+
+// Exercício 2 - Herança
+var objeto2D = {
+    base: 0,
+    altura: 0
+}
+ 
+var retangulo = Object.create(objeto2D)
+retangulo.base = 5
+retangulo.altura = 7
+retangulo.area = function() {
+    return this.base * this.altura
+}
+console.log(retangulo.area())
+ 
+// Exercício 3 - Getters & Setters
+var estagiario = {
+    _primeiroNome: ''
+}
+ 
+Object.defineProperty(estagiario, 'primeiroNome', {
+    get: function () {
+        return this._primeiroNome
+    },
+    set: function (valor) {
+        if (valor.length >= 3) {
+            this._primeiroNome = valor
+        } else {
+            this._primeiroNome = ''
+        }
+    },
+    enumerable: true,
+    configurable: true
+})
+ 
+// console.log(estagiario.primeiroNome)
+// estagiario.primeiroNome = 'Le'
+// console.log(estagiario.primeiroNome)
+// estagiario.primeiroNome = 'Leonardo'
+// console.log(estagiario.primeiroNome)
